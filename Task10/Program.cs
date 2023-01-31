d5d001211e268e7c0fcb458a56f5e5902442d02f
thenewlife <thenewlife@mail.ru>
2023-01-31 18:38:47 +0300

    Added homework2

---
+++ Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = numberRequest("Введите число:");
+
+// показать вторую цифру числа
+int ShowSecondDigit (int number)
+{                                 
+    int SecondDigit = ((number / 10) % 10);
+    return SecondDigit;
+}
+
+int SecondDigit = ShowSecondDigit(number);
+
+Console.WriteLine(SecondDigit);
---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = numberRequest("Введите число:");
+
+
+if(number<=5 && number>=1)
+{
+    Console.WriteLine("Нет!");
+}
+else if(number>=6 && number<=7)
+{
+ Console.WriteLine("Да!");
+ }
+
+else Console.WriteLine("Как вы знаете, в неделе всего 7 дней");
